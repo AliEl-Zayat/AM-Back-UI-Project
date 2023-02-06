f6f64d865c88cf769dedd69d8c8989859eb2207b
Ali El-Zayat <alielzayat07@gmail.com>
2023-02-06 15:17:06 -0800

    Initialized The App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  isRouteErrorResponse,
+  RouterProvider,
+} from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+
+import Root from "./Pages/Root";
+import ErrorPage from "./Pages/ErrorPage";
+import Home from "./Pages/Home";
 
 const router = createBrowserRouter([
   {
-    element,
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/home", element: <></> },
+    ],
   },
 ]);
 
